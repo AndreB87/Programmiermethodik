6fa7a98b43e2525b988de4549cbdc396e0612f06
Andre Brand <andre.brand@yahoo.de>
2016-10-20 10:39:54 +0200

    automatisch eingefuegte, boedsinnige Zeichenkombinationen entfernt

--- aufgabenblatt2/StreamVerarbeitung.java
+++ aufgabenblatt2/StreamVerarbeitung.java
@@ -3,10 +3,7 @@ package aufgabenblatt2;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-<<<<<<< HEAD
 import java.util.Objects;
-=======
->>>>>>> 7b80d1931d2316f8594142e676e133510964922a
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -15,11 +12,8 @@ public class StreamVerarbeitung {
 
 	public static List<String> streamVerarbeiten(String... strings ) {
 		Stream<String> stringStream = Arrays.stream(strings);
-<<<<<<< HEAD
 		stringStream = stringStream.filter(Objects::nonNull);
-=======
 		stringStream = stringStream.filter(StreamVerarbeitung::isNotNull);
->>>>>>> 7b80d1931d2316f8594142e676e133510964922a
 		stringStream = stringStream.map(string -> string = string.trim().toUpperCase()
 				.replaceAll("Ä", "AE")
 				.replaceAll("Ö", "OE")
@@ -31,8 +25,6 @@ public class StreamVerarbeitung {
 		return stringList;
 	}
 	
-<<<<<<< HEAD
-=======
 	private static boolean isNotNull(Object obj) {
 		if (obj != null) {
 			return true;
@@ -40,7 +32,6 @@ public class StreamVerarbeitung {
 		return false;
 	}
 	
->>>>>>> 7b80d1931d2316f8594142e676e133510964922a
 	private static String stringKuerzen(String string) {
 		if (string.length() > MAX_STRING_LENGTH) {
 			string = string.substring(0, MAX_STRING_LENGTH - 1);
